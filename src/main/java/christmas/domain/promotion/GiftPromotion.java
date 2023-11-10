dd2077dc800f0f3a653b045bcf4a718617998814
doha <dohaahi@naver.com>
2023-11-11 02:25:16 +0900

    feat (GiftPromotion): 증정 이벤트 도메인 구현

---
+++ src/main/java/christmas/domain/promotion/GiftPromotion.java
@@ -0,0 +1,9 @@
+package christmas.domain.promotion;
+
+import christmas.domain.MenuItem;
+
+public class GiftPromotion {
+    private static final int MIN_ORDER_AMOUNT = 120_000;
+    private final MenuItem giftItem = MenuItem.CHAMPAGNE;
+    private final int giftCount = 1;
+}
