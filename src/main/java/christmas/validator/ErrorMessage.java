e50b91b6ab4e7d3aac55b0910a39d8710a102d34
doha <dohaahi@naver.com>
2023-11-11 00:04:48 +0900

    refactor (ErrorMessage): 최대 주문 가능 메뉴 수 초과한 경우 메세지 추가

--- src/main/java/christmas/validator/ErrorMessage.java
+++ src/main/java/christmas/validator/ErrorMessage.java
@@ -3,8 +3,9 @@ package christmas.validator;
 public enum ErrorMessage {
     ENTER_VALUE_MASSAGE("값을 입력해주세요."),
     INVALID_INPUT_NUMBER_MESSAGE("숫자를 입력해주세요."),
+    INVALID_INPUT_DATE_MESSAGE("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
     INVALID_INPUT_ORDER_MENU_MESSAGE("유효하지 않은 주문입니다. 다시 입력해 주세요."),
-    INVALID_INPUT_DATE_MESSAGE("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    INVALID_INPUT_ORDER_MENU_COUNT_MESSAGE("최대로 주문 가능한 메뉴는 20개 입니다. 다시 입력해 주세요.");
 
     private final String errorFormat = "[ERROR] ";
     private final String message;
