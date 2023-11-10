56b6d4abc05df63752543d035343c4422de7b888
doha <dohaahi@naver.com>
2023-11-10 10:58:23 +0900

    feat (Date): 해당 달의 최소날짜, 최대날짜 검증하는 기능 구현

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -1,6 +1,8 @@
 package christmas.domain;
 
 public class Date {
+    public static final int MIN_DATE_IN_MONTH = 1;
+    public static final int MAX_DATE_IN_MONTH = 31;
     private final int date;
 
     public Date(int date) {
--- src/main/java/christmas/validator/DateValidator.java
+++ src/main/java/christmas/validator/DateValidator.java
@@ -1,5 +1,7 @@
 package christmas.validator;
 
+import static christmas.domain.Date.MAX_DATE_IN_MONTH;
+import static christmas.domain.Date.MIN_DATE_IN_MONTH;
 import static christmas.validator.InputValidator.validateDateMatchedNumberRegex;
 import static christmas.validator.InputValidator.validateValueEmpty;
 
@@ -7,5 +9,17 @@ public class DateValidator {
     public static void validateInputDate(final String input) {
         validateValueEmpty(input);
         validateDateMatchedNumberRegex(input);
+    }
+
+    public static void validateDate(final String input) {
+        validateDateInMonth(input);
+    }
+
+    private static void validateDateInMonth(final String input) {
+        final int day = Integer.parseInt(input);
+
+        if (MIN_DATE_IN_MONTH > day || MAX_DATE_IN_MONTH < day) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT_DATE_MESSAGE.getMessage());
+        }
     }
 }
