1f0696d5b593022fe214b90260b62140d4386e86
doha <dohaahi@naver.com>
2023-11-10 13:18:08 +0900

    refactor (InputView): 방문 날짜 입력 받는 메소드 반환 타입 변경 DateDto -> Date

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -3,19 +3,24 @@ package christmas.view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static christmas.validator.DateValidator.validateInputDate;
 
-import christmas.domain.dto.DateDto;
+import christmas.domain.Date;
+import christmas.domain.dto.OrderMenuDto;
+import christmas.domain.dto.OrderMenusDto;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputView {
     private static final String GREETING_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n";
     private static final String EXPECTED_VISIT_DATE_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n";
     private static final String ORDER_MENU_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n";
 
-    public DateDto inputVisitDate() {
+    public Date inputVisitDate() {
         System.out.println(GREETING_MESSAGE);
         System.out.println(EXPECTED_VISIT_DATE_MESSAGE);
         final String input = readLine().trim();
         validateInputDate(input);
 
-        return new DateDto(Integer.parseInt(input));
+        return new Date(Integer.parseInt(input));
     }
 }
