d6709b4e85db843978035f95f280052d9ca5c933
doha <dohaahi@naver.com>
2023-11-10 17:12:02 +0900

    feat (OrderMenus): 주문 메뉴들 도메인 구현

---
+++ src/main/java/christmas/domain/OrderMenus.java
@@ -0,0 +1,31 @@
+package christmas.domain;
+
+import static christmas.domain.util.StringConverter.delimiterStringToList;
+import static christmas.validator.OrderMenusValidator.validateInputOrderMenus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderMenus {
+    public static final String DELIMITER = ", ";
+    private final List<OrderMenu> menus;
+
+    public OrderMenus(final String input) {
+        validateInputOrderMenus(input);
+
+        List<String> orderMenus = delimiterStringToList(DELIMITER, input);
+        List<OrderMenu> menus = converterStringToMenus(orderMenus);
+
+        this.menus = menus;
+    }
+
+    private List<OrderMenu> converterStringToMenus(List<String> orderMenus) {
+        List<OrderMenu> menus = new ArrayList<>();
+
+        orderMenus.forEach(orderMenu -> {
+            OrderMenu menu = OrderMenu.from(orderMenu);
+            menus.add(menu);
+        });
+        return menus;
+    }
+}
