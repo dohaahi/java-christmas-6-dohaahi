0c9694c99b112347bbe4d9bc0d1e7b9db7e52cf1
doha <dohaahi@naver.com>
2023-11-10 17:09:40 +0900

    refactor (StringConverter): 클래스명 수정 StringConvertor -> StringConverter

--- src/main/java/christmas/domain/util/StringConverter.java
+++ src/main/java/christmas/domain/util/StringConverter.java
@@ -3,7 +3,7 @@ package christmas.domain.util;
 import java.util.Arrays;
 import java.util.List;
 
-public class StringConvertor {
+public class StringConverter {
     public static final List<String> delimiterStringToList(final String delimiter, final String input) {
         return Arrays.stream(input.split(delimiter)).toList();
     }
