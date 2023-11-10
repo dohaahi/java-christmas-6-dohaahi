29b1d756d50366436901f6738c78f1b17960bb8f
doha <dohaahi@naver.com>
2023-11-11 03:34:48 +0900

    refactor (Menus): 메뉴 목록을 갖는 필드 타입 변경 List -> Map

--- src/main/java/christmas/domain/Menus.java
+++ src/main/java/christmas/domain/Menus.java
@@ -3,19 +3,28 @@ package christmas.domain;
 import static christmas.util.StringConverter.delimiterStringToList;
 import static christmas.validator.OrderMenusValidator.validateOrderMenus;
 
+import christmas.domain.menu.MenuItem;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Menus {
     public static final String DELIMITER = ", ";
-    private final List<Menu> menus;
+    private final Map<MenuItem, Integer> menus;
 
     public Menus(final String input) {
         List<String> orderMenus = delimiterStringToList(DELIMITER, input);
         List<Menu> menus = converterStringToMenus(orderMenus);
         validateOrderMenus(menus);
 
-        this.menus = menus;
+        Map<MenuItem, Integer> menuStorage = new HashMap<>();
+        menus.forEach(menu -> {
+            menuStorage.put(MenuItem.getMenuItem(menu), menu.getMenuCount());
+        });
+
+        this.menus = menuStorage;
     }
 
     private List<Menu> converterStringToMenus(List<String> orderMenus) {
