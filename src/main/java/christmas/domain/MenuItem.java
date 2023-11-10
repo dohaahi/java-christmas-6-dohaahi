dbb3454d1611e65fd9739d8d416b8ab894b58e27
doha <dohaahi@naver.com>
2023-11-11 02:15:35 +0900

    refactor (MenuItem): 메뉴 목록에 카테고리 필드 추가

--- src/main/java/christmas/domain/MenuItem.java
+++ src/main/java/christmas/domain/MenuItem.java
@@ -3,25 +3,27 @@ package christmas.domain;
 import java.util.List;
 
 public enum MenuItem {
-    MUSHROOM_SOUP("양송이수프", 6_000),
-    TAPAS("타파스", 5_500),
-    CAESAR_SALAD("시저샐러드", 8_000),
-    T_BONE_STEAK("티본스테이크", 55_000),
-    BARBECUE_LIP("바비큐립", 54_000),
-    SEAFOOD_PASTA("해산물파스타", 35_000),
-    CHRISTMAS_PASTA("크리스마스파스타", 25_000),
-    CHOCO_CAKE("초코케이크", 15_000),
-    ICE_CREAM("아이스크림", 5_000),
-    ZERO_COLA("제로콜라", 3_000),
-    RED_WINE("레드와인", 60_000),
-    CHAMPAGNE("샴페인", 25_000);
+    MUSHROOM_SOUP("양송이수프", 6_000, MenuCategory.APPETIZER),
+    TAPAS("타파스", 5_500, MenuCategory.APPETIZER),
+    CAESAR_SALAD("시저샐러드", 8_000, MenuCategory.APPETIZER),
+    T_BONE_STEAK("티본스테이크", 55_000, MenuCategory.MAIN),
+    BARBECUE_LIP("바비큐립", 54_000, MenuCategory.MAIN),
+    SEAFOOD_PASTA("해산물파스타", 35_000, MenuCategory.MAIN),
+    CHRISTMAS_PASTA("크리스마스파스타", 25_000, MenuCategory.MAIN),
+    CHOCO_CAKE("초코케이크", 15_000, MenuCategory.DESSERT),
+    ICE_CREAM("아이스크림", 5_000, MenuCategory.DESSERT),
+    ZERO_COLA("제로콜라", 3_000, MenuCategory.DRINK),
+    RED_WINE("레드와인", 60_000, MenuCategory.DRINK),
+    CHAMPAGNE("샴페인", 25_000, MenuCategory.DRINK);
 
     private final String menuName;
     private final int menuPrice;
+    private final MenuCategory categoryName;
 
-    MenuItem(String menuName, int menuPrice) {
+    MenuItem(String menuName, int menuPrice, MenuCategory categoryName) {
         this.menuName = menuName;
         this.menuPrice = menuPrice;
+        this.categoryName = categoryName;
     }
 
     private static List<MenuItem> menuItems = List.of(
@@ -39,12 +41,12 @@ public enum MenuItem {
             CHAMPAGNE
     );
 
-    public static boolean isMatchMenu(final OrderMenu menu) {
+    public static boolean isMatchMenu(final Menu menu) {
         return menuItems.stream()
                 .anyMatch(menuItem -> menu.getMenuName().equals(menuItem.menuName));
     }
 
-    public String getMenuName() {
-        return menuName;
+    public MenuCategory getCategoryName() {
+        return categoryName;
     }
 }
