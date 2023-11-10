191527dd01bdcf539472cebdd821d7c8313a9006
doha <dohaahi@naver.com>
2023-11-11 02:24:49 +0900

    feat (SpecialPromotion): 특별 할인 도메인 구현

---
+++ src/main/java/christmas/domain/promotion/SpecialPromotion.java
@@ -0,0 +1,11 @@
+package christmas.domain.promotion;
+
+import christmas.domain.DayInStar;
+
+public class SpecialPromotion {
+    private final DayInStar dayInStar;
+
+    public SpecialPromotion(DayInStar dayInStar) {
+        this.dayInStar = dayInStar;
+    }
+}
