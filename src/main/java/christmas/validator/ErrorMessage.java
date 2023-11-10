6da7e7c2da74b1e3ec4ff9eef87f98867d0b2fe0
doha <dohaahi@naver.com>
2023-11-10 10:57:59 +0900

    refactor (ErrorMessage): 날짜 입력이 잘못된 경우에 해당하는 오류 메세지 작성

--- src/main/java/christmas/validator/ErrorMessage.java
+++ src/main/java/christmas/validator/ErrorMessage.java
@@ -3,7 +3,8 @@ package christmas.validator;
 public enum ErrorMessage {
     ENTER_VALUE_MASSAGE("값을 입력해주세요."),
     INVALID_INPUT_NUMBER_MESSAGE("숫자를 입력해주세요."),
-    INVALID_INPUT_ORDER_MENU_MESSAGE("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    INVALID_INPUT_ORDER_MENU_MESSAGE("유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    INVALID_INPUT_DATE_MESSAGE("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
 
     private final String errorFormat = "[ERROR] ";
     private final String message;
