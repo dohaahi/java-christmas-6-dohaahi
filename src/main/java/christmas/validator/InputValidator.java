a51368cdcbdcd223f4943f57f0962299b2b44b1f
doha <dohaahi@naver.com>
2023-11-10 16:35:30 +0900

    refactor (InputValidator): 주문 메뉴 정규식 수정

--- src/main/java/christmas/validator/InputValidator.java
+++ src/main/java/christmas/validator/InputValidator.java
@@ -1,10 +1,11 @@
 package christmas.validator;
 
+import christmas.domain.OrderMenu;
 import java.util.regex.Pattern;
 
 public class InputValidator {
     public static final String NUMBER_REGEX = "^[\\d]*$";
-    public static final String MENU_ORDER_REGEX = "^[ㄱ-ㅎ|가-힣]+-[\\d]*$";
+    public static final String ORDER_MENU_REGEX = "^[가-힣]+" + OrderMenu.DELIMITER + "[\\d]*$";
 
     public static void validateValueEmpty(final String input) {
         if (input.isEmpty()) {
@@ -18,8 +19,8 @@ public class InputValidator {
         }
     }
 
-    public static void validateMenuOrderMatchedMenuOrderRegex(final String input) {
-        if (!Pattern.matches(MENU_ORDER_REGEX, input)) {
+    public static void validateOrderMenuMatchedMenuOrderRegex(final String input) {
+        if (!Pattern.matches(ORDER_MENU_REGEX, input)) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT_ORDER_MENU_MESSAGE.getMessage());
         }
     }
