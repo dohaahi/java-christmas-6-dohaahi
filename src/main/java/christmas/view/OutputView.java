0741686997e5cf1685078ed7e79e3219662ed74e
doha <dohaahi@naver.com>
2023-11-10 23:09:38 +0900

    feat (OutputView): 혜택 미리 보기 메세지 출력 기능 구현

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,11 @@
+package christmas.view;
+
+import static christmas.domain.Date.CURRENT_MONTH;
+
+public class OutputView {
+    public static final String PREVIEW_EVENT_MESSAGE = CURRENT_MONTH + "월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+
+    public void printPreviewEvent(final int date) {
+        System.out.printf(PREVIEW_EVENT_MESSAGE, date);
+    }
+}
