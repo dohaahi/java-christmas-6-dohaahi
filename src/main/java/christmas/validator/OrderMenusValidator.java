86dc4d3224f0d18dc52494acc78cd4bccfc6dbe8
doha <dohaahi@naver.com>
2023-11-10 17:09:09 +0900

    feat (OrderMenusValidator): 메뉴 주문 입력에 대한 검증 기능 구현

--- src/main/java/christmas/validator/OrderMenusValidator.java
+++ src/main/java/christmas/validator/OrderMenusValidator.java
@@ -1,14 +1,14 @@
 package christmas.validator;
 
-import static christmas.domain.util.StringConvertor.delimiterStringToList;
+import static christmas.domain.util.StringConverter.delimiterStringToList;
 import static christmas.validator.InputValidator.validateOrderMenuMatchedMenuOrderRegex;
 import static christmas.validator.InputValidator.validateValueEmpty;
 
 import christmas.domain.OrderMenus;
 import java.util.List;
 
-public class OrderMenuValidator {
-    public static void validateInputOrderMenu(final String input) {
+public class OrderMenusValidator {
+    public static void validateInputOrderMenus(final String input) {
         validateValueEmpty(input);
         List<String> orderMenus = delimiterStringToList(OrderMenus.DELIMITER, input);
 
