0038e57a52736234326d149be3d4302697eae839
doha <dohaahi@naver.com>
2023-11-11 02:22:24 +0900

    feat (WeekendPromotion): 주말 할인 도메인 구현

---
+++ src/main/java/christmas/domain/promotion/WeekendPromotion.java
@@ -0,0 +1,11 @@
+package christmas.domain.promotion;
+
+import christmas.domain.DayOfWeek;
+import christmas.domain.MenuCategory;
+
+public class WeekendPromotion {
+    private final DayOfWeek startPeriod = DayOfWeek.SUNDAY;
+    private final DayOfWeek endPeriod = DayOfWeek.THURSDAY;
+    private final MenuCategory discountItem = MenuCategory.DESSERT;
+    private final int discountAmount = 2_023;
+}
