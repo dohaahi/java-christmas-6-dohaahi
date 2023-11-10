10906f4cc6e0b1c8536a1ab06b325543d87114c6
doha <dohaahi@naver.com>
2023-11-10 22:52:55 +0900

    refactor (Date): 날짜 검증 메소드 추가

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -1,11 +1,15 @@
 package christmas.domain;
 
+import static christmas.validator.DateValidator.validateDate;
+
 public class Date {
     public static final int MIN_DATE_IN_MONTH = 1;
     public static final int MAX_DATE_IN_MONTH = 31;
     private final int date;
 
     public Date(int date) {
+        validateDate(date);
+
         this.date = date;
     }
 }
