4a18f2c56e674ef075d8bb11bd23731d5d7b4a98
doha <dohaahi@naver.com>
2023-11-10 10:58:54 +0900

    test (DateValidatorTest): 해당 달의 최소날짜, 최대날짜 검증하는 메소드 test 작성

--- src/test/java/christmas/validator/DateValidatorTest.java
+++ src/test/java/christmas/validator/DateValidatorTest.java
@@ -1,5 +1,8 @@
 package christmas.validator;
 
+import static christmas.domain.Date.MAX_DATE_IN_MONTH;
+import static christmas.domain.Date.MIN_DATE_IN_MONTH;
+import static christmas.validator.DateValidator.validateDate;
 import static christmas.validator.DateValidator.validateInputDate;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -52,6 +55,50 @@ class DateValidatorTest {
             assertThatThrownBy(() -> validateInputDate(input2))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage(ErrorMessage.INVALID_INPUT_NUMBER_MESSAGE.getMessage());
+        }
+    }
+
+    @Nested
+    @DisplayName("validateDate 메소드 test")
+    class ValidateDate {
+        @DisplayName("날짜가 " + MIN_DATE_IN_MONTH + "~" + MAX_DATE_IN_MONTH + " 범위에 포함 되면 검증 통과")
+        @Test
+        void Is_Date_In_Month() {
+            // given
+            String input1 = "1";
+            String input2 = "15";
+            String input3 = "31";
+
+            // when
+            // then
+            validateDate(input1);
+            validateDate(input2);
+            validateDate(input3);
+        }
+
+        @DisplayName("날짜가 " + MIN_DATE_IN_MONTH + "~" + MAX_DATE_IN_MONTH + " 범위에 포함 되지 않으면 예외 발생")
+        @Test
+        void Is_Date_Not_In_Month() {
+            // given
+            String input1 = "0";
+            String input2 = "32";
+            String input3 = "-1";
+            String input4 = "100";
+
+            // when
+            // then
+            assertThatThrownBy(() -> validateDate(input1))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.INVALID_INPUT_DATE_MESSAGE.getMessage());
+            assertThatThrownBy(() -> validateDate(input2))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.INVALID_INPUT_DATE_MESSAGE.getMessage());
+            assertThatThrownBy(() -> validateDate(input3))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.INVALID_INPUT_DATE_MESSAGE.getMessage());
+            assertThatThrownBy(() -> validateDate(input4))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.INVALID_INPUT_DATE_MESSAGE.getMessage());
         }
     }
 }
