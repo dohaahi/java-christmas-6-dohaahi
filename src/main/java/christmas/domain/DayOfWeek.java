4bd03fe8d16bdd5db16b106d02d9ad07dd090485
doha <dohaahi@naver.com>
2023-11-11 02:16:10 +0900

    feat (DayOfWeek): 요일 목록 enum 작성

---
+++ src/main/java/christmas/domain/DayOfWeek.java
@@ -0,0 +1,16 @@
+package christmas.domain;
+
+public enum DayOfWeek {
+    SUNDAY("일요일"),
+    MONDAY("월요일"),
+    TUESDAY("화요일"),
+    WEDNESDAY("수요일"),
+    THURSDAY("목요일"),
+    FRIDAY("금요일"),
+    SATURDAY("토요일");
+    private final String day;
+
+    DayOfWeek(String day) {
+        this.day = day;
+    }
+}
