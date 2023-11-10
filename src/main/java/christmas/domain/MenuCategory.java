4e2e1f770a36d93055108a0362fd54cc1a3294da
doha <dohaahi@naver.com>
2023-11-11 02:14:35 +0900

    feat (MenuCategory): 메뉴 카테고리 enum 생성

---
+++ src/main/java/christmas/domain/MenuCategory.java
@@ -0,0 +1,13 @@
+package christmas.domain;
+
+public enum MenuCategory {
+    APPETIZER("애피타이저"),
+    MAIN("메인"),
+    DESSERT("디저트"),
+    DRINK("음료");
+    private final String categoryName;
+
+    MenuCategory(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
