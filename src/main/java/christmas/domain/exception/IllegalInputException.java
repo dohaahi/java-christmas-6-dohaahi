1221d25e88e215a861086bacfeba009ecf0beb2e
doha <dohaahi@naver.com>
2023-11-12 02:47:10 +0900

    feat (IllegalInputException): 입력에 대한 예외 구현

---
+++ src/main/java/christmas/domain/exception/IllegalInputException.java
@@ -0,0 +1,9 @@
+package christmas.domain.exception;
+
+public class IllegalInputException extends IllegalArgumentException {
+    private static final String EXCEPTION_MESSAGE = "유효하지 않은 입력입니다. 다시 입력해 주세요.";
+
+    public IllegalInputException() {
+        super(EXCEPTION_MESSAGE);
+    }
+}
