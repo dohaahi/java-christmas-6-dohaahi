b0c195de7aca43570483e0515920e8377847a272
doha <dohaahi@naver.com>
2023-11-11 21:23:28 +0900

    refactor (ChristmasPromotion): 크리스마스가 지나면 할인되지 않도록 수정

--- src/main/java/christmas/domain/promotion/ChristmasPromotion.java
+++ src/main/java/christmas/domain/promotion/ChristmasPromotion.java
@@ -4,13 +4,16 @@ import christmas.domain.Date;
 import christmas.domain.Menus;
 
 public class ChristmasPromotion implements Promotion {
-    private static final int PROMOTION_START_DATE = 1;
     public static final int CHRISTMAS_DATE = 25;
     private static final int DISCOUNT_AMOUNT_INIT = 1_000;
     private static final int DAILY_DISCOUNT_AMOUNT = 100;
 
     @Override
     public int discountAmount(final Menus menus, final Date date) {
+        if (date.getDate() > CHRISTMAS_DATE) {
+            return NO_DISCOUNT;
+        }
+
         return DISCOUNT_AMOUNT_INIT + (date.getDate() * DAILY_DISCOUNT_AMOUNT);
     }
 }
