f3417a6b3df5d6a213147bfb77a4bff687b563d9
doha <dohaahi@naver.com>
2023-11-12 03:46:34 +0900

    refactor (MenuItems): menus 필드 타입 변경 Map -> List

--- src/main/java/christmas/domain/MenuItems.java
+++ src/main/java/christmas/domain/MenuItems.java
@@ -1,11 +1,8 @@
 package christmas.domain;
 
-import static christmas.util.StringConverter.delimiterStringToList;
 import static christmas.validator.MenusValidator.validateOrderMenus;
 
 import christmas.domain.menu.Menu;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -14,34 +11,12 @@ public class MenuItems {
     public static final String DELIMITER = ", ";
     public static final int MIN_ORDER_COUNT = 1;
     public static final int MAX_ORDER_COUNT = 20;
-    private final Map<Menu, Integer> menus;
 
-    public MenuItems(final String input) {
-        List<String> orderMenus = delimiterStringToList(DELIMITER, input);
-        List<MenuItem> menuItems = converterStringToMenus(orderMenus);
+    private final List<MenuItem> menuItems;
+
+    public MenuItems(final List<MenuItem> menuItems) {
         validateOrderMenus(menuItems);
-
-        Map<Menu, Integer> menuStorage = new HashMap<>();
-        menuItems.forEach(menu -> {
-            menuStorage.put(Menu.getMenuItem(menu), menu.getCount());
-        });
-
-        this.menus = menuStorage;
-    }
-
-    private List<MenuItem> converterStringToMenus(List<String> orderMenus) {
-        List<MenuItem> menuItems = new ArrayList<>();
-
-        try {
-            orderMenus.forEach(orderMenu -> {
-                MenuItem menuItem = MenuItem.from(orderMenu);
-                menuItems.add(menuItem);
-            });
-        } catch (IllegalArgumentException exception) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT_ORDER_MENU_MESSAGE.getMessage());
-        }
-
-        return menuItems;
+        this.menuItems = menuItems;
     }
 
     public int getTotalPrice() {
