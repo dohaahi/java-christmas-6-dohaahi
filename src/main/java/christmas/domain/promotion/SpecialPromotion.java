545d047eb2e26ac3707c6adff5acb7c5ba632fad
doha <dohaahi@naver.com>
2023-11-11 21:25:12 +0900

    feat (PeriodPromotion): 별이 있는 경우 할인되는 기능 구현

--- src/main/java/christmas/domain/promotion/SpecialPromotion.java
+++ src/main/java/christmas/domain/promotion/SpecialPromotion.java
@@ -1,18 +1,23 @@
 package christmas.domain.promotion;
 
+import static christmas.domain.promotion.ChristmasPromotion.CHRISTMAS_DATE;
+
 import christmas.domain.Date;
-import christmas.domain.DayInStar;
 import christmas.domain.Menus;
 
 public class SpecialPromotion implements Promotion {
-    private final DayInStar dayInStar;
-
-    public SpecialPromotion(DayInStar dayInStar) {
-        this.dayInStar = dayInStar;
-    }
+    public static final int SPECIAL_DISCOUNT_AMOUNT = 1_000;
 
     @Override
     public int discountAmount(Menus menus, Date date) {
-        return 0;
+        if (!isValidSpecialDiscountPeriod(date)) {
+            return NO_DISCOUNT;
+        }
+
+        return SPECIAL_DISCOUNT_AMOUNT;
+    }
+
+    private boolean isValidSpecialDiscountPeriod(final Date date) {
+        return CHRISTMAS_DATE == date.getDate() || date.getDayOfWeek().equals("Sunday");
     }
 }
