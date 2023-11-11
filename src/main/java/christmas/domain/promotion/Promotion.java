032d23ac3bf3c038fbd1d543f4c29089c6bfa783
doha <dohaahi@naver.com>
2023-11-11 21:16:34 +0900

    refactor (Promotion): 아무 할인이 안된 할인액 상수 추가

--- src/main/java/christmas/domain/promotion/Promotion.java
+++ src/main/java/christmas/domain/promotion/Promotion.java
@@ -4,5 +4,7 @@ import christmas.domain.Date;
 import christmas.domain.Menus;
 
 public interface Promotion {
+    public static final int NO_DISCOUNT = 0;
+
     public int discountAmount(final Menus menus, final Date date);
 }
