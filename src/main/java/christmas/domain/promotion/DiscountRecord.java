01f6faa50344d74eb69e767d76c5464540ed4365
doha <dohaahi@naver.com>
2023-11-13 00:03:51 +0900

    feat (DiscountRecord): 할인 내역을 갖는 도메인 구현

---
+++ src/main/java/christmas/domain/promotion/DiscountRecord.java
@@ -0,0 +1,49 @@
+package christmas.domain.promotion;
+
+import java.util.List;
+
+public class DiscountRecord {
+    private final int christmasPromotionDiscountAmount;
+    private final int periodPromotionDiscountAmount;
+    private final int specialPromotionDiscountAmount;
+    private final int giftPromotionDiscountAmount;
+    private final int totalDiscountAmount;
+
+    public DiscountRecord(int christmasPromotionDiscountAmount, int periodPromotionDiscountAmount,
+                          int specialPromotionDiscountAmount, int giftPromotionDiscountAmount
+    ) {
+        this.christmasPromotionDiscountAmount = christmasPromotionDiscountAmount;
+        this.periodPromotionDiscountAmount = periodPromotionDiscountAmount;
+        this.specialPromotionDiscountAmount = specialPromotionDiscountAmount;
+        this.giftPromotionDiscountAmount = giftPromotionDiscountAmount;
+
+        final List<Integer> discountAmounts = List.of(christmasPromotionDiscountAmount,
+                periodPromotionDiscountAmount, specialPromotionDiscountAmount, giftPromotionDiscountAmount);
+
+        this.totalDiscountAmount = calculateDiscountAmount(discountAmounts);
+    }
+
+    private int calculateDiscountAmount(final List<Integer> discountAmounts) {
+        return discountAmounts.stream().mapToInt(Integer::intValue).sum();
+    }
+
+    public int getChristmasPromotionDiscountAmount() {
+        return christmasPromotionDiscountAmount;
+    }
+
+    public int getPeriodPromotionDiscountAmount() {
+        return periodPromotionDiscountAmount;
+    }
+
+    public int getSpecialPromotionDiscountAmount() {
+        return specialPromotionDiscountAmount;
+    }
+
+    public int getGiftPromotionDiscountAmount() {
+        return giftPromotionDiscountAmount;
+    }
+
+    public int calculateDiscountAmount() {
+        return totalDiscountAmount;
+    }
+}
