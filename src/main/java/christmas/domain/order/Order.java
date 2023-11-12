9c79b623f80ce158b3c82270b025c32ba6e5aea8
doha <dohaahi@naver.com>
2023-11-13 02:41:24 +0900

    feat (Order): 주문 할인 기능 구현

--- src/main/java/christmas/domain/order/Order.java
+++ src/main/java/christmas/domain/order/Order.java
@@ -2,13 +2,22 @@ package christmas.domain.order;
 
 import christmas.domain.date.Date;
 import christmas.domain.menu.MenuItems;
+import christmas.domain.promotion.PromotionProcessor;
+import christmas.domain.promotion.PromotionRecord;
 
 public class Order {
     private final Date date;
     private final MenuItems menus;
+    private final PromotionProcessor promotionProcessor;
 
     public Order(Date date, MenuItems menus) {
         this.date = date;
         this.menus = menus;
+
+        promotionProcessor = new PromotionProcessor(date, menus);
+    }
+
+    public PromotionRecord discount() {
+        return promotionProcessor.discount();
     }
 }
