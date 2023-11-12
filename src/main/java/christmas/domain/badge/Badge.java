4c576c85e10c16ebdba0aa9c2047cb660973cff1
doha <dohaahi@naver.com>
2023-11-13 02:41:51 +0900

    feat (Badge): 배지 enum 구현

---
+++ src/main/java/christmas/domain/badge/Badge.java
@@ -0,0 +1,23 @@
+package christmas.domain.badge;
+
+public enum Badge {
+    STAR("별", 5_000),
+    TREE("트리", 10_000),
+    SANTA("산타", 20_000),
+    NONE("없음", 0);
+    private final String name;
+    private final int minPrice;
+
+    Badge(String name, int minPrice) {
+        this.name = name;
+        this.minPrice = minPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMinPrice() {
+        return minPrice;
+    }
+}
