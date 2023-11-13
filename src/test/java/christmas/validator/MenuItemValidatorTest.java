a777d41da64843c659786c706219e0ddc911b0e1
doha <dohaahi@naver.com>
2023-11-13 13:01:35 +0900

    test (MenuItemValidatorTest): 메뉴아이템 생성자 변경에 따른 테스트 코드 수정

--- src/test/java/christmas/validator/MenuItemValidatorTest.java
+++ src/test/java/christmas/validator/MenuItemValidatorTest.java
@@ -3,7 +3,8 @@ package christmas.validator;
 import static christmas.domain.menu.MenuItems.MIN_ORDER_COUNT;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import christmas.domain.menu.MenuItems;
+import christmas.domain.menu.MenuItem;
+import christmas.exception.IllegalMenuException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,16 +14,14 @@ class MenuItemValidatorTest {
     void Order_Count_Under_Min() {
         // given
         String menus1 = "시저샐러드-0";
-        String menus2 = "양송이수프--1, 크리스마스파스타-3, 제로콜라-1";
+        String menus2 = "양송이수프--1";
 
         // when
         // then
-        assertThatThrownBy(() -> new MenuItems(menus1))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_INPUT_ORDER_MENU_MESSAGE.getMessage());
-        assertThatThrownBy(() -> new MenuItems(menus2))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_INPUT_ORDER_MENU_MESSAGE.getMessage());
+        assertThatThrownBy(() -> MenuItem.from(menus1))
+                .isInstanceOf(IllegalMenuException.class);
+        assertThatThrownBy(() -> MenuItem.from(menus2))
+                .isInstanceOf(IllegalMenuException.class);
     }
 
     @DisplayName("메뉴 목록에 존재하는 메뉴를 시키면 검증 통과")
@@ -30,12 +29,12 @@ class MenuItemValidatorTest {
     void Order_Menu_In_Menu_Item() {
         // given
         String menus1 = "양송이수프-1";
-        String menus2 = "양송이수프-1, 크리스마스파스타-3, 제로콜라-1";
+        String menus2 = "크리스마스파스타-3";
 
         // when
         // then
-        new MenuItems(menus1);
-        new MenuItems(menus2);
+        MenuItem.from(menus1);
+        MenuItem.from(menus2);
     }
 
     @DisplayName("메뉴 목록에 존재하지 않는 메뉴를 시키면 예외 발생")
@@ -43,15 +42,13 @@ class MenuItemValidatorTest {
     void Order_Menu_Not_In_Menu_Item() {
         // given
         String menus1 = "브로콜리수프-1";
-        String menus2 = "브로콜리수프-1, 윈터파스타-3, 아메리카노-1";
+        String menus2 = "브로콜리수프-1";
 
         // when
         // then
-        assertThatThrownBy(() -> new MenuItems(menus1))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_INPUT_ORDER_MENU_MESSAGE.getMessage());
-        assertThatThrownBy(() -> new MenuItems(menus2))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_INPUT_ORDER_MENU_MESSAGE.getMessage());
+        assertThatThrownBy(() -> MenuItem.from(menus1))
+                .isInstanceOf(IllegalMenuException.class);
+        assertThatThrownBy(() -> MenuItem.from(menus2))
+                .isInstanceOf(IllegalMenuException.class);
     }
 }
