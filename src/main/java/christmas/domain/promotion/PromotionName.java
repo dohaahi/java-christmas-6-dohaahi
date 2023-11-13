46f2cbd6cd5fc75273f7da3764a794d5fb7dfd3c
doha <dohaahi@naver.com>
2023-11-14 00:38:15 +0900

    feat (PromotionName): 프로모션 이름 enum 생성

---
+++ src/main/java/christmas/domain/promotion/PromotionName.java
@@ -0,0 +1,18 @@
+package christmas.domain.promotion;
+
+public enum PromotionName {
+    CHRISTMAS_PROMOTION("크리스마스 디데이 할인"),
+    WEEKEND_PROMOTION("주말 할인"),
+    WEEKDAY_PROMOTION("평일 할인"),
+    SPECIAL_PROMOTION("특별 할인"),
+    GIFT_PROMOTION("증정 이벤트");
+    private final String name;
+
+    PromotionName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
