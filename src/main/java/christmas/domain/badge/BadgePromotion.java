67ba33a33cdc0523ccd0fd960d2383ac2e7aa5b7
doha <dohaahi@naver.com>
2023-11-14 00:36:09 +0900

    refactor (BadgePromotion): 배지 매치하는 기능 수정 총 주문 금액 비교 -> 총 할인 금액 비교

--- src/main/java/christmas/domain/badge/BadgePromotion.java
+++ src/main/java/christmas/domain/badge/BadgePromotion.java
@@ -1,22 +1,33 @@
 package christmas.domain.badge;
 
-import christmas.domain.menu.MenuItems;
+import christmas.domain.promotion.PromotionRecord;
 
 public class BadgePromotion {
+    private final Badge badge;
 
-    public Badge present(final MenuItems menuItems) {
-        if (menuItems.totalPrice() > Badge.SANTA.getMinPrice()) {
+    public BadgePromotion(final PromotionRecord promotionRecord) {
+        this.badge = matchBadgeFrom(promotionRecord);
+    }
+
+    public Badge matchBadgeFrom(final PromotionRecord promotionRecord) {
+        int totalDiscountAmount = promotionRecord.getTotalDiscountAmount();
+
+        if (totalDiscountAmount > Badge.SANTA.getMinPrice()) {
             return Badge.STAR;
         }
 
-        if (menuItems.totalPrice() > Badge.TREE.getMinPrice()) {
+        if (totalDiscountAmount > Badge.TREE.getMinPrice()) {
             return Badge.TREE;
         }
 
-        if (menuItems.totalPrice() > Badge.STAR.getMinPrice()) {
+        if (totalDiscountAmount > Badge.STAR.getMinPrice()) {
             return Badge.SANTA;
         }
 
         return Badge.NONE;
+    }
+
+    public Badge getBadge() {
+        return badge;
     }
 }
