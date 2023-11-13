157c49efc15bb5936b169c9d51d49555ad520a80
doha <dohaahi@naver.com>
2023-11-14 00:40:18 +0900

    feat (PromotionRecordDto): 할인 내역 dto 생성

---
+++ src/main/java/christmas/domain/dto/PromotionRecordDto.java
@@ -0,0 +1,9 @@
+package christmas.domain.dto;
+
+public record PromotionRecordDto(int christmasPromotionDiscountAmount,
+                                 int periodPromotionDiscountAmount,
+                                 int specialPromotionDiscountAmount,
+                                 int giftPromotionDiscountAmount,
+                                 int totalDiscountAmount
+) {
+}
