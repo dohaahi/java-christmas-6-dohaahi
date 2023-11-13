5b555f150423efa10acc660f557551f91dfd2ebb
doha <dohaahi@naver.com>
2023-11-14 00:52:38 +0900

    feat (Application): 이벤트 플래너 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,15 @@
 package christmas;
 
+import christmas.controller.EventPlanner;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        EventPlanner promotionController = new EventPlanner(inputView, outputView);
+
+        promotionController.makeReservation();
     }
 }
