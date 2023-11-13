555b486924507c69f1771da243c4adc72ae512cd
doha <dohaahi@naver.com>
2023-11-14 00:44:24 +0900

    feat (OrderRecordMapper): 도메인을 dto로 변환하는 기능 구현

--- src/main/java/christmas/controller/OrderRecordMapper.java
+++ src/main/java/christmas/controller/OrderRecordMapper.java
@@ -1,30 +1,56 @@
 package christmas.controller;
 
+import static christmas.domain.promotion.GiftPromotion.GIFT_MENU;
+
 import christmas.domain.OrderRecord;
 import christmas.domain.badge.BadgePromotion;
+import christmas.domain.dto.BadgePromotionDto;
+import christmas.domain.dto.OrderDto;
+import christmas.domain.dto.PromotionRecordDto;
 import christmas.domain.order.Order;
+import christmas.domain.promotion.GiftPromotion;
 import christmas.domain.promotion.PromotionRecord;
 
 public class OrderRecordMapper {
-    /*
-        주문 메뉴 - Order - menuItems
-        할인 전 총 주문 금액 - Order - menuItems
-        증정 메뉴 - Order - promotionProcessor
-        혜택 내역 - PromotionRecord
-        총 혜택 금액 - PromotionRecord
-        할인 후 결제 예상 금액 - PromotionRecord
-        12월 이벤트 배지 - Badge <- menuItems: totalPrice
-     */
     private final Order order;
     private final PromotionRecord promotionRecord;
-    private final BadgePromotion badgePromotion = new BadgePromotion();
+    private final BadgePromotion badgePromotion;
 
     public OrderRecordMapper(Order order, PromotionRecord promotionRecord) {
         this.order = order;
         this.promotionRecord = promotionRecord;
+        this.badgePromotion = new BadgePromotion(promotionRecord);
     }
 
     public OrderRecord toOrderRecord() {
-        return new OrderRecord(order, promotionRecord, badgePromotion);
+        return new OrderRecord(makeOrderDto(), makePromotionRecordDto(), makeBadgePromotionDto());
+    }
+
+    private OrderDto makeOrderDto() {
+        return new OrderDto(order.getDate(), order.getMenus(), order.getPromotionProcessor());
+    }
+
+    private PromotionRecordDto makePromotionRecordDto() {
+        int totalDiscountAmount = promotionRecord.getTotalDiscountAmount();
+
+        return new PromotionRecordDto(
+                promotionRecord.getChristmasPromotionDiscountAmount(),
+                promotionRecord.getPeriodPromotionDiscountAmount(),
+                promotionRecord.getSpecialPromotionDiscountAmount(),
+                promotionRecord.getGiftPromotionDiscountAmount(),
+                totalDiscountMapper(totalDiscountAmount)
+        );
+    }
+
+    private int totalDiscountMapper(final int totalDiscountAmount) {
+        if (GiftPromotion.isGiftPresent(totalDiscountAmount)) {
+            return totalDiscountAmount - GIFT_MENU.getPrice();
+        }
+
+        return totalDiscountAmount;
+    }
+
+    private BadgePromotionDto makeBadgePromotionDto() {
+        return new BadgePromotionDto(badgePromotion.getBadge());
     }
 }
