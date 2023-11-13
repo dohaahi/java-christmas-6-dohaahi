af826e1537f711e65de0253962aa5c69a8f93b76
doha <dohaahi@naver.com>
2023-11-14 00:30:13 +0900

    refactor (Menu): 이름 getter 추가

--- src/main/java/christmas/domain/menu/Menu.java
+++ src/main/java/christmas/domain/menu/Menu.java
@@ -4,12 +4,18 @@ import christmas.exception.IllegalMenuException;
 import java.util.List;
 
 public enum Menu {
-    MUSHROOM_SOUP("양송이수프", 6_000, MenuCategory.APPETIZER), TAPAS("타파스", 5_500, MenuCategory.APPETIZER), CAESAR_SALAD(
-            "시저샐러드", 8_000, MenuCategory.APPETIZER), T_BONE_STEAK("티본스테이크", 55_000, MenuCategory.MAIN), BARBECUE_LIP(
-            "바비큐립", 54_000, MenuCategory.MAIN), SEAFOOD_PASTA("해산물파스타", 35_000, MenuCategory.MAIN), CHRISTMAS_PASTA(
-            "크리스마스파스타", 25_000, MenuCategory.MAIN), CHOCO_CAKE("초코케이크", 15_000, MenuCategory.DESSERT), ICE_CREAM(
-            "아이스크림", 5_000, MenuCategory.DESSERT), ZERO_COLA("제로콜라", 3_000, MenuCategory.DRINK), RED_WINE("레드와인",
-            60_000, MenuCategory.DRINK), CHAMPAGNE("샴페인", 25_000, MenuCategory.DRINK);
+    MUSHROOM_SOUP("양송이수프", 6_000, MenuCategory.APPETIZER),
+    TAPAS("타파스", 5_500, MenuCategory.APPETIZER),
+    CAESAR_SALAD("시저샐러드", 8_000, MenuCategory.APPETIZER),
+    T_BONE_STEAK("티본스테이크", 55_000, MenuCategory.MAIN),
+    BARBECUE_LIP("바비큐립", 54_000, MenuCategory.MAIN),
+    SEAFOOD_PASTA("해산물파스타", 35_000, MenuCategory.MAIN),
+    CHRISTMAS_PASTA("크리스마스파스타", 25_000, MenuCategory.MAIN),
+    CHOCO_CAKE("초코케이크", 15_000, MenuCategory.DESSERT),
+    ICE_CREAM("아이스크림", 5_000, MenuCategory.DESSERT),
+    ZERO_COLA("제로콜라", 3_000, MenuCategory.DRINK),
+    RED_WINE("레드와인", 60_000, MenuCategory.DRINK),
+    CHAMPAGNE("샴페인", 25_000, MenuCategory.DRINK);
 
     private final static List<Menu> MENUS = List.of(values());
     private final String name;
@@ -28,6 +34,10 @@ public enum Menu {
 
     public static boolean isMatchMenu(final String menuName) {
         return MENUS.stream().anyMatch(menuItem -> menuName.equals(menuItem.name));
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getPrice() {
