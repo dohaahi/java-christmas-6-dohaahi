66935e108590832302d4e5d23c194de45b7e4095
doha <dohaahi@naver.com>
2023-11-15 05:07:52 +0900

    feat (PromotionProcessor): 프로모션 처리하는 기능 구현

---
+++ src/main/java/christmas/domain/promotion/PromotionProcessor.java
@@ -0,0 +1,58 @@
+package christmas.domain.promotion;
+
+import christmas.domain.date.Date;
+import christmas.domain.menu.MenuItems;
+import christmas.domain.order.OrderElement;
+import java.util.List;
+
+public class PromotionProcessor {
+    private final OrderElement orderElement;
+    private final BadgePromotion badgePromotion = new BadgePromotion();
+    private List<Promotion> promotions;
+
+    public PromotionProcessor(final Date date, final MenuItems menuItems) {
+        this.orderElement = new OrderElement(date, menuItems);
+
+        if (date.isWeekend(date)) {
+            weekendPromotions();
+            return;
+        }
+
+        weekdayPromotions();
+    }
+
+    private void weekendPromotions() {
+        promotions = List.of(
+                new ChristmasPromotion(),
+                new WeekendPromotion(),
+                new SpecialPromotion(),
+                new GiftPromotion());
+    }
+
+    private void weekdayPromotions() {
+        promotions = List.of(
+                new ChristmasPromotion(),
+                new WeekdayPromotion(),
+                new SpecialPromotion(),
+                new GiftPromotion());
+    }
+
+    public int getTotalDiscountAmount() {
+        return promotions.stream()
+                .mapToInt(promotion -> promotion.discountAmount(orderElement))
+                .sum();
+    }
+
+    public int getDiscountAmount(final Promotion promotion) {
+        return promotion.discountAmount(orderElement);
+    }
+
+    public String getBadge(final int totalDiscountAmount) {
+        return badgePromotion.getBadge(totalDiscountAmount)
+                .name();
+    }
+
+    public List<Promotion> getPromotions() {
+        return promotions;
+    }
+}
