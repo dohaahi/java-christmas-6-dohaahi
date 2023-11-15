dca2c9016ac3fb034750c2842050caf26881991c
doha <dohaahi@naver.com>
2023-11-15 23:04:56 +0900

    refactor (PromotionsMapper): 할인 내역이 없으면 할인내역이 출력되지 않도록 수정

--- src/main/java/christmas/mapper/PromotionsMapper.java
+++ src/main/java/christmas/mapper/PromotionsMapper.java
@@ -11,11 +11,18 @@ public class PromotionsMapper {
         final List<Promotion> promotions = promotionProcessor.getPromotions();
 
         return promotions.stream()
+                .filter(promotion -> promotionProcessor.getDiscountAmount(promotion) != 0)
                 .map(promotion -> toPromotionDto(promotionProcessor, promotion))
                 .toList();
     }
 
     private static PromotionDto toPromotionDto(final PromotionProcessor promotionProcessor, final Promotion promotion) {
+        int discountAmount = promotionProcessor.getDiscountAmount(promotion);
+
+        if (discountAmount == 0) {
+            return null;
+        }
+
         return new PromotionDto(promotion.getName(), promotionProcessor.getDiscountAmount(promotion));
     }
 }
